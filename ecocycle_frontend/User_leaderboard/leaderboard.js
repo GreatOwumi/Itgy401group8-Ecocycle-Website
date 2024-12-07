b71f0f1a6221a95e5a4625ab3767a3bc2b012386
Taversh <126422279+Taversh@users.noreply.github.com>
2024-12-07 21:12:43 +0100

    Add files via upload

--- ecocycle_frontend/User_leaderboard/leaderboard.js
+++ ecocycle_frontend/User_leaderboard/leaderboard.js
@@ -1,27 +1,25 @@
+ // Timer logic
+ function startTimer(duration, display) {
+  var timer = duration, hours, minutes, seconds;
+  setInterval(function () {
+    hours = parseInt(timer / 3600, 10);
+    minutes = parseInt((timer % 3600) / 60, 10);
+    seconds = parseInt(timer % 60, 10);
 
-    // Timer logic
-    function startTimer(duration, display) {
-      var timer = duration, hours, minutes, seconds;
-      setInterval(function () {
-        hours = parseInt(timer / 3600, 10);
-        minutes = parseInt((timer % 3600) / 60, 10);
-        seconds = parseInt(timer % 60, 10);
+    hours = hours < 10 ? "0" + hours : hours;
+    minutes = minutes < 10 ? "0" + minutes : minutes;
+    seconds = seconds < 10 ? "0" + seconds : seconds;
 
-        hours = hours < 10 ? "0" + hours : hours;
-        minutes = minutes < 10 ? "0" + minutes : minutes;
-        seconds = seconds < 10 ? "0" + seconds : seconds;
+    display.textContent = hours + ":" + minutes + ":" + seconds;
 
-        display.textContent = hours + ":" + minutes + ":" + seconds;
-
-        if (--timer < 0) {
-          timer = duration; // Reset the timer when it finishes
-        }
-      }, 1000);
+    if (--timer < 0) {
+      timer = duration; // Reset the timer when it finishes
     }
+  }, 1000);
+}
 
-    window.onload = function () {
-      var timeLimit = 60 * 60 * 3; // 3 hours
-      var display = document.querySelector('#timer');
-      startTimer(timeLimit, display); // Start the timer
-    };
-  
+window.onload = function () {
+  var timeLimit = 60 * 60 * 3; // 3 hours
+  var display = document.querySelector('#timer');
+  startTimer(timeLimit, display); // Start the timer
+};
